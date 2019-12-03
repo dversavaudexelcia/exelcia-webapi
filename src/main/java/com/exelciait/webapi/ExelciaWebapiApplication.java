535cc86585b24296213a60ef002516eccd2252e0
dversavaud <dversavaud@PC-EXL-RUN-08.mpi.grp>
2019-12-03 16:14:04 +0400

    Commit initial

---
+++ src/main/java/com/exelciait/webapi/ExelciaWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.exelciait.webapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExelciaWebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExelciaWebapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exelciait/webapi/ExelciaWebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exelciait.webapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExelciaWebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
