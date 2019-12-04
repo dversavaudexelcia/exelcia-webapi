77e94525b266bb394af29137bd2da08ee3eb1f0a
dversavaud <dversavaud@PC-EXL-RUN-08.mpi.grp>
2019-12-04 13:55:27 +0400

    test modif application.java

--- src/main/java/com/exelcia/webapi/ExelciaWebapiApplication.java
+++ src/main/java/com/exelcia/webapi/ExelciaWebapiApplication.java
@@ -2,11 +2,18 @@ package com.exelcia.webapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class ExelciaWebapiApplication {
+public class ExelciaWebapiApplication extends SpringBootServletInitializer {
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ExelciaWebapiApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExelciaWebapiApplication.class, args);
