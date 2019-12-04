fc12b067169e3626e018b3880d2e5ea10dbf51c3
dversavaud <dversavaud@PC-EXL-RUN-08.mpi.grp>
2019-12-04 18:57:40 +0400

    Correction

--- src/main/java/exelcia/webapi/services/UserAuthService.java
+++ src/main/java/exelcia/webapi/services/UserAuthService.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.exelcia.webapi.exceptions.ResourceNotFoundException;
 import com.exelcia.webapi.model.User;
 import com.exelcia.webapi.repository.UserRepository;
 
+@Service
 public class UserAuthService implements UserDetailsService {
 
 	@Autowired
