ddd617645f316692e82051c970fe9b7fcc2c1c16
dversavaud <dversavaud@PC-EXL-RUN-08.mpi.grp>
2019-12-04 11:54:50 +0400

    Ajout de la suppression

--- src/main/java/com/exelcia/webapi/controller/ForfaitController.java
+++ src/main/java/com/exelcia/webapi/controller/ForfaitController.java
@@ -2,11 +2,17 @@ package com.exelcia.webapi.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exelcia.webapi.exceptions.ResourceNotFoundException;
@@ -34,5 +40,19 @@ public class ForfaitController {
 		return repo.findById(forfaitId).orElseThrow(
 				() -> new ResourceNotFoundException("Forfait", "id", forfaitId));
 	}
-
+	
+	@PostMapping("/Forfaits")
+	public Forfait createForfait(@Valid @RequestBody Forfait forfait) {
+		return repo.save(forfait);
+	}
+	
+	@DeleteMapping("/forfaits/{id}")
+	public ResponseEntity<Forfait> deleteForfaitById(@PathVariable(value="id")Long forfaitId) {
+		Forfait forfait = repo.findById(forfaitId).orElseThrow(
+				() -> new ResourceNotFoundException("Forfait", "id", forfaitId));
+		repo.delete(forfait);
+		return ResponseEntity.ok(forfait);
+	}
+	
+	
 }
