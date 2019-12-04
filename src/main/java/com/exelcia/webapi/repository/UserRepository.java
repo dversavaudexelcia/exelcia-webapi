f62d1432bb7f547470b49e1c73a5e10bac1c0a22
dversavaud <dversavaud@PC-EXL-RUN-08.mpi.grp>
2019-12-04 19:04:16 +0400

    Correction

--- src/main/java/com/exelcia/webapi/repository/UserRepository.java
+++ src/main/java/com/exelcia/webapi/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.exelcia.webapi.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.exelcia.webapi.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	Optional<User> findUserByUsername(String username);
